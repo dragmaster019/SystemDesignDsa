6055fe50faea9ab9af39b8b7f19d93360a2186a4
Sarthak mondal <83837160+dragmaster019@users.noreply.github.com>
2026-09-12 17:28:05 +0530

    leetcode 111.java

--- RealWorldLld/mockTest/mock02/treebfs/111/111.java
+++ RealWorldLld/mockTest/mock02/treebfs/111/111.java
@@ -24,3 +24,91 @@
 //
 // - -1000 <= Node.val <= 1000
 // LEETCODE-PROBLEM-END
+
+import java.util.*;
+
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+
+    TreeNode(){}
+
+    TreeNode(int val){
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution{
+
+    public int avgLevel(TreeNode root){
+
+        if(root == null) return 0;
+
+        Queue<TreeNode> q = new ArrayDeque<>();
+        int level = 1;
+
+        q.offer(root);
+
+        while(!q.isEmpty()){
+
+            int size = q.size();
+            
+
+            for(int i =0; i< size; i++){
+
+                TreeNode temp = q.poll();
+
+
+                // Leaf node
+                if (temp.left == null && temp.right == null) {
+                    return level;
+                }
+
+               
+
+                if(temp.left!=null){
+                    q.offer(temp.left);
+                }
+
+                if(temp.right!= null){
+                    q.offer(temp.right);
+                }
+
+            }
+
+            level++;
+
+        }
+
+        return level;
+    }
+
+    public static void main(String[] args){
+
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        Solution sol = new Solution();
+
+        int result = sol.avgLevel(root);
+
+        System.out.println(result);
+    }
+    
+
+}
+
+
+
