9be9320f945d7a194afaed1ea1cc88ea069d70fd
Sarthak mondal <83837160+dragmaster019@users.noreply.github.com>
2026-09-16 20:12:23 +0530

    pathSum

--- RealWorldLld/mockTest/mock02/dfs/104/112/112.java
+++ RealWorldLld/mockTest/mock02/dfs/104/112/112.java
@@ -35,3 +35,61 @@
 //
 // - -1000 <= targetSum <= 1000
 // LEETCODE-PROBLEM-END
+
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(){}
+
+    TreeNode(int val){
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution{
+
+    public boolean hasPathSum(TreeNode root, int targetSum){
+        
+        if(root == null) return false;
+
+        if(root.left == null && root.right == null){
+            return targetSum == root.val;
+        }
+
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+
+    public static void main(String[] args){
+
+        TreeNode root = new TreeNode(5);
+
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(8);
+
+        root.left.left = new TreeNode(11);
+
+        root.right.left = new TreeNode(13);
+        root.right.right = new TreeNode(4);
+
+        root.left.left.left = new TreeNode(7);
+        root.left.left.right = new TreeNode(2);
+
+        root.right.right.right = new TreeNode(1);
+
+        int targetSum = 22;
+
+        Solution sol = new Solution();
+
+        boolean result = sol.hasPathSum(root, targetSum);
+
+        System.out.println(result);
+    }
+}
