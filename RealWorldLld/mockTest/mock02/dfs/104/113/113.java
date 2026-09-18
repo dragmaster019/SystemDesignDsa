8c0a5f267ae6abb5d30ff28d6381be0645b462f3
Sarthak mondal <83837160+dragmaster019@users.noreply.github.com>
2026-09-18 13:25:53 +0530

    path sum 2

--- RealWorldLld/mockTest/mock02/dfs/104/113/113.java
+++ RealWorldLld/mockTest/mock02/dfs/104/113/113.java
@@ -33,21 +33,70 @@
 // - -1000 <= targetSum <= 1000
 // LEETCODE-PROBLEM-END
 
-class TreeNode{
+import java.util.*;
+
+class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
 
-    TreeNode(){}
+    TreeNode() {}
 
-    TreeNode(int val){
+    TreeNode(int val) {
         this.val = val;
     }
 
-    TreeNode(int val, TreeNode left, TreeNode right){
+    TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
         this.right = right;
+    }
+}
+
+class Solution {
+
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+
+        List<Integer> path = new ArrayList<>();
+        dfs(root, targetSum, path, result);
+        return result;
+    }
+
+    private void dfs(TreeNode root, int targetSum, List<Integer> path, List<List<Integer>> result) {
+        if (root == null) return;
+
+        path.add(root.val);
+
+        if (root.left == null && root.right == null && targetSum == root.val) {
+            result.add(new ArrayList<>(path));
+        } else {
+            dfs(root.left, targetSum - root.val, path, result);
+            dfs(root.right, targetSum - root.val, path, result);
+        }
+
+        path.remove(path.size() - 1);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(5);
+
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(8);
+
+        root.left.left = new TreeNode(11);
+        root.right.left = new TreeNode(13);
+        root.right.right = new TreeNode(4);
+        root.left.left.left = new TreeNode(7);
+        root.left.left.right = new TreeNode(2);
+
+        root.right.right.left = new TreeNode(5);
+        root.right.right.right = new TreeNode(1);
+
+        Solution s = new Solution();
+        int targetSum = 22;
+        System.out.println(s.pathSum(root, targetSum));
     }
 }
 
